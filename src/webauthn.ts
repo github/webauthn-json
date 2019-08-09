bcc6f3117f19dd5ee2198dc2f2caddb7883261a9
Lucas Garron <lgarron@github.com>
2019-08-09 15:47:10 -0700

    Fix documentation: s/public credential/public key credential/

--- src/webauthn.ts
+++ src/webauthn.ts
@@ -22,7 +22,7 @@ declare global {
 }
 
 // This function does a simple check to test for the credential management API
-// functions we need, and an indication of public credential authentication
+// functions we need, and an indication of public key credential authentication
 // support.
 // https://developers.google.com/web/updates/2018/03/webauthn-credential-management
 export function supported(): boolean {
