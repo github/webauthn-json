54ff8689f20e47522d455064d1b81138a6b8f82f
Lucas Garron <lgarron@github.com>
2019-09-20 17:19:54 -0700

    Update `window.PublicKeyCredential` type to be `PublicKeyCredential`.

--- src/webauthn.ts
+++ src/webauthn.ts
@@ -21,7 +21,7 @@ export async function get(requestJSON: CredentialRequestOptionsJSON): Promise<Pu
 
 declare global {
   interface Window {
-    PublicKeyCredential: any | undefined;
+    PublicKeyCredential: PublicKeyCredential | undefined;
   }
 }
 
