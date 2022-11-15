bacf8ef37a8dcf710a8b47dd157caccedf676532
Lucas Garron <lgarron@github.com>
2022-11-14 17:00:36 -0800

    Update to `rome` stable.
    Includes workarounds for:
    - https://github.com/rome/tools/issues/3734
    - https://github.com/rome/tools/issues/3735

--- src/webauthn-json/basic/supported.ts
+++ src/webauthn-json/basic/supported.ts
@@ -4,7 +4,9 @@
 // https://developers.google.com/web/updates/2018/03/webauthn-credential-management
 
 export function supported(): boolean {
+  // rome-ignore format: Work around https://github.com/rome/tools/issues/3734
   return !!(
+    // rome-ignore lint(style/useOptionalChain): Optional chaining creates more complicated ES2019 code
     navigator.credentials &&
     navigator.credentials.create &&
     navigator.credentials.get &&
